d9aa70ae37b832ee49a5e6b3b532fe11575d1ec3
Lucas <lucas@yesbabylon.com>
2024-10-28 16:47:54 +0100

    Specific sale booking do quote for lodging

--- sale/actions/booking/do-quote.php
+++ sale/actions/booking/do-quote.php
@@ -117,35 +117,11 @@ Booking::id($params['id'])->update(['is_price_tbc' => false, 'price' => null, 't
 $om->callonce(BookingLine::getType(), '_updatePriceId', $booking['booking_lines_ids']);
 
 
-// in case rental units were freed, check if consistency must be maintained with channel manager (if booking impacts a rental unit that is linked to a channelmanager room type)
+// in case rental units were freed
 if($params['free_rental_units']) {
-
-    $booking = Booking::id($params['id'])
-        ->read(['date_from', 'date_to', 'consumptions_ids' => ['is_accomodation', 'rental_unit_id']])
-        ->first(true);
-
-    $map_rental_units_ids = [];
-    foreach($booking['consumptions_ids'] as $consumption) {
-        if($consumption['is_accomodation']) {
-            $map_rental_units_ids[$consumption['rental_unit_id']] = true;
-        }
-    }
 
     // remove consumptions if requested (link & part)
     Consumption::search(['booking_id', '=', $params['id']])->delete(true);
-
-    if(count($map_rental_units_ids)) {
-        $cron->schedule(
-                "channelmanager.check-contingencies.{$params['id']}",
-                time(),
-                'lodging_booking_check-contingencies',
-                [
-                    'date_from'         => date('c', $booking['date_from']),
-                    'date_to'           => date('c', $booking['date_to']),
-                    'rental_units_ids'  => array_keys($map_rental_units_ids)
-                ]
-            );
-    }
 }
 
 // remove pending alerts relating to booking checks, if any
