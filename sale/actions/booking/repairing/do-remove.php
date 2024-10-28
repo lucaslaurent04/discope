fb2bc26a68dc0eee3880269159dc4e4441405f38
Lucas <lucas@yesbabylon.com>
2024-10-28 16:36:17 +0100

    Specific sale booking repairing do remove and booking do cancel

--- sale/actions/booking/repairing/do-remove.php
+++ sale/actions/booking/repairing/do-remove.php
@@ -35,37 +35,8 @@ list($params, $providers) = announce([
  */
 list($context, $orm, $cron) = [$providers['context'], $providers['orm'], $providers['cron']];
 
-$repairing = Repairing::id($params['id'])
-    ->read(['date_from', 'date_to', 'rental_units_ids'])
-    ->first(true);
-
-// remember impacted rental units
-$map_rental_units_ids = [];
-
-foreach($repairing['rental_units_ids'] as $rental_unit_id) {
-    $map_rental_units_ids[$rental_unit_id] = true;
-}
-
 // remove targeted repairing
 Repairing::id($params['id'])->delete(true);
-
-/*
-    Check if consistency must be maintained with channel manager (if repairing impacts a rental unit that is linked to a channelmanager room type)
-*/
-
-if(count($map_rental_units_ids)) {
-    $cron->schedule(
-            "channelmanager.check-contingencies.{$params['id']}",
-            time(),
-            'lodging_booking_check-contingencies',
-            [
-                'date_from'         => date('c', $repairing['date_from']),
-                // repairings completely cover the last day of the date range
-                'date_to'           => date('c', strtotime('+1 day', $repairing['date_to'])),
-                'rental_units_ids'  => array_keys($map_rental_units_ids)
-            ]
-        );
-}
 
 $context->httpResponse()
         // success but notify client to reset content
