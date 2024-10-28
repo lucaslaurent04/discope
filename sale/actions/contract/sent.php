537657849902bb3e83453f697b226c933f629e78
Lucas <lucas@yesbabylon.com>
2024-10-28 10:10:46 +0100

    Move action booking contract

--- sale/actions/contract/sent.php
+++ sale/actions/contract/sent.php
@@ -1,42 +1,43 @@
 <?php
 /*
-    This file is part of Symbiose Community Edition <https://github.com/yesbabylon/symbiose>
-    Some Rights Reserved, Yesbabylon SRL, 2020-2021
-    Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
+    This file is part of the Discope property management software.
+    Author: Yesbabylon SRL, 2020-2024
+    License: GNU AGPL 3 license <http://www.gnu.org/licenses/>
 */
-use sale\contract\Contract;
 
-use core\Task;
+use sale\booking\Contract;
 
-list($params, $providers) = announce([
+[$params, $providers] = eQual::announce([
     'description'   => "Mark a contract as sent to the customer.",
     'params'        => [
         'id' =>  [
-            'description'   => 'Identifier of the targeted contract.',
             'type'          => 'integer',
+            'description'   => "Identifier of the targeted contract.",
             'min'           => 1,
             'required'      => true
         ]
     ],
-    'access' => [
-        'visibility'        => 'public',		// 'public' (default) or 'private' (can be invoked by CLI only)		
-        'groups'            => ['booking.default.user'],// list of groups ids or names granted 
+    'access'        => [
+        'visibility'    => 'public',
+        'groups'        => ['booking.default.user'],
     ],
     'response'      => [
         'content-type'  => 'application/json',
         'charset'       => 'utf-8',
         'accept-origin' => '*'
     ],
-    'providers'     => ['context', 'orm', 'auth']     
+    'providers'     => ['context']
 ]);
 
-list($context, $orm, $auth) = [$providers['context'], $providers['orm'], $providers['auth']];
+/**
+ * @var \equal\php\Context  $context
+ */
+['context' => $context] = $providers;
 
-// read contract object
 $contract = Contract::id($params['id'])
-                  ->read(['id', 'name', 'status', 'valid_until'])
-                  ->first();
-                  
+    ->read(['id', 'name', 'status', 'valid_until'])
+    ->first(true);
+
 if(!$contract) {
     throw new Exception("unknown_contract", QN_ERROR_UNKNOWN_OBJECT);
 }
@@ -45,12 +46,11 @@ if($contract['status'] != 'pending') {
     throw new Exception("invalid_status", QN_ERROR_NOT_ALLOWED);
 }
 
-// Update booking status
 Contract::id($params['id'])->update(['status' => 'sent']);
-
 
 // #todo - check if required payment have been paid in the meantime
 
 $context->httpResponse()
-        ->status(204)
+        ->status(200)
+        ->body([])
         ->send();
