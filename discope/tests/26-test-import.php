409a589899e381f248ce4bde2af9a415f958d3aa
alexandraYesbabylon <alexandra@yesbabylon.com>
2024-11-28 15:26:55 +0000

    Add Verification of the bank statement import for payment processing

--- discope/tests/26-test-import.php
+++ discope/tests/26-test-import.php
@@ -155,17 +155,74 @@ $tests = [
                 ]);
             }
             catch(Exception $e) {
-                $message = $e->getMessage();
+                $e->getMessage();
             }
 
-            return $message ;
-        },
-        'assert'            =>  function ($message) {
+            $bank_statement_line = BankStatementLine::search(['structured_message' , '=', $PAYMENT_REFERENCE])
+                ->read( ['id', 'amount'])
+                ->first(true);
 
-            return (isset($message));
+            $booking = Booking::id($booking['id'])
+                ->update(['status' => 'confirmed'])
+                ->read(['id', 'price', 'center_office_id'])
+                ->first(true);
+
+            Funding::search(['booking_id'  ,'=',  $booking['id']])->update(['state' => 'archive']);
+
+            Funding::create([
+                    'booking_id'        =>  $booking['id'],
+                    'due_amount'        =>  $bank_statement_line['amount'],
+                    'center_office_id'  =>  $booking['center_office_id']
+                ])
+                ->update(['payment_reference' => $PAYMENT_REFERENCE])
+                ->read(['id', 'due_amount'])
+                ->first(true);
+
+            try {
+                eQual::run('do', 'sale_pay_bankstatementline_do-reconcile', ['id' => $bank_statement_line['id']]);
+            }
+            catch(Exception $e) {
+                $e->getMessage();
+            }
+
+            $booking = Booking::id($booking['id'])
+                ->read(['id','status' , 'payment_reference', 'price', 'total',
+                            'fundings_ids' => [
+                                'id','booking_id', 'center_office_id', 'payment_reference', 'is_paid', 'paid_amount', 'due_amount',
+                                'payments_ids' => [
+                                    'id' , 'statement_line_id', 'payment_origin', 'payment_method'
+                                ]
+                            ]
+                ])
+                ->first(true);
+
+
+            return $booking;
+        },
+        'assert'            =>  function ($booking) {
+
+            $bankStatementLine = BankStatementLine::search(['structured_message', '=', $booking['payment_reference']])
+                ->read(['id', 'status'])
+                ->first(true);
+
+            return $bankStatementLine['status'] === 'reconciled' &&
+                $booking['fundings_ids'][0]['is_paid'] &&
+                $booking['fundings_ids'][0]['paid_amount'] === $booking['fundings_ids'][0]['due_amount'] &&
+                $booking['fundings_ids'][0]['payments_ids'][0]['payment_method'] === 'wire_transfer' &&
+                $booking['fundings_ids'][0]['payments_ids'][0]['payment_origin'] === 'bank';
 
         },
         'rollback'          =>  function () {
+
+            $booking = Booking::search(['description', 'like', '%'. 'Automatic bank statement line reconciliation'.'%'])
+                ->update(['status' => 'quote'])
+                ->read(['id'])
+                ->first(true);
+
+            Payment::search(['booking_id' ,'=', $booking['id']])
+                ->update(['state' => 'archive']);
+
+            Booking::id($booking['id'])->delete(true);
 
         }
     ]
