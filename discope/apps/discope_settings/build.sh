a25de276b966d9d35ca4e8ca41507bddfb3c0429
Cédric Françoys <cedricfrancoys@gmail.com>
2024-12-18 16:49:11 +0100

    restored comments Lin/Win

--- discope/apps/discope_settings/build.sh
+++ discope/apps/discope_settings/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 rm -rf .angular
+# Windows : npm link is necessary to link sb-shared-lib from the global node_modules folder
 npm link sb-shared-lib
-ng build --configuration production --output-hashing none --base-href="/discope_settings\\"
+# Windows : ng build --configuration production --output-hashing none --base-href="//discope_settings\\"
+# Linux : ng build --configuration production --base-href="/discope_settings/"
+ng build --configuration production --output-hashing none --base-href="//discope_settings\\"
 touch manifest.json && rm -f web.app && cp manifest.json dist/symbiose/ && cd dist/symbiose && zip -r ../../web.app * && cd ../..
 cat web.app | md5sum | awk '{print $1}' > version
 cat version
