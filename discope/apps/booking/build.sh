c761ad67498a7e3d75b0e8a4fdf35fc23afc4fac
Cédric Françoys <cedricfrancoys@gmail.com>
2024-12-18 16:49:30 +0100

    restored comments Lin/Win

--- discope/apps/booking/build.sh
+++ discope/apps/booking/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 rm -rf .angular
+# Windows : npm link is necessary to link sb-shared-lib from the global node_modules folder
 npm link sb-shared-lib
-ng build --configuration production --output-hashing none --base-href="/booking\\"
+# Windows : ng build --configuration production --output-hashing none --base-href="//booking\\"
+# Linux : ng build --configuration production --base-href="/booking/"
+ng build --configuration production --output-hashing none --base-href="//booking\\"
 touch manifest.json && rm -f web.app && cp manifest.json dist/symbiose/ && cd dist/symbiose && zip -r ../../web.app * && cd ../..
 cat web.app | md5sum | awk '{print $1}' > version
 cat version
