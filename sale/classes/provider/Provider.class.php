c609484a327789de6d4e55719fdb685bf0fdefac
alexandraYesbabylon <alexandra@yesbabylon.com>
2025-02-07 15:51:12 +0000

    Add provider class

---
+++ sale/classes/provider/Provider.class.php
@@ -0,0 +1,115 @@
+<?php
+/*
+    This file is part of the Discope property management software <https://github.com/discope-pms/discope>
+    Some Rights Reserved, Discope PMS, 2020-2024
+    Original author(s): Yesbabylon SRL
+    Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
+*/
+namespace sale\provider;
+
+
+class Provider extends \identity\Partner {
+
+    public static function getName() {
+        return 'Provider';
+    }
+
+    public static function getColumns() {
+
+        return [
+
+            'relationship' => [
+                'type'              => 'string',
+                'default'           => 'provider',
+                'description'       => 'Force relationship to Provider.'
+            ],
+
+            'type_id' => [
+                'type'              => 'many2one',
+                'foreign_object'    => 'identity\IdentityType',
+                'default'           => 3,
+                'description'       => 'Type of identity.',
+            ],
+
+            'address' => [
+                'type'              => 'computed',
+                'result_type'       => 'string',
+                'function'          => 'calcAddress',
+                'description'       => 'Main address from related Identity.',
+                'store'             => true
+            ],
+
+
+            'vat_number' => [
+                'type'              => 'computed',
+                'result_type'       => 'string',
+                'description'       => 'Value Added Tax identification number, if any.',
+                'function'          => 'calcVatNumber',
+                'store'             => true
+            ],
+
+            'registration_number' => [
+                'type'              => 'computed',
+                'result_type'       => 'string',
+                'description'       => 'Organisation registration number (company number).',
+                'function'          => 'calcRegistrationNumber',
+                'store'             => true
+            ],
+
+            'partner_identity_id' => [
+                'type'              => 'many2one',
+                'foreign_object'    => 'identity\Identity',
+                'description'       => 'The targeted identity (the partner).',
+                'onupdate'          => 'onupdatePartnerIdentityId',
+                'required'          => true
+            ],
+
+            'product_models_ids' => [
+                'type'              => 'many2many',
+                'foreign_object'    => 'sale\catalog\ProductModel',
+                'foreign_field'     => 'providers_ids',
+                'rel_table'         => 'sale_catalog_product_model_rel_sale_provider_providers',
+                'rel_foreign_key'   => 'product_model_id',
+                'rel_local_key'     => 'provider_id',
+                'description'       => "The product models that can be reauired providers.",
+            ]
+        ];
+    }
+
+
+    /**
+     * Computes the number of bookings made by the provider during the last two years.
+     *
+     */
+    public static function calcAddress($om, $oids, $lang) {
+        $result = [];
+
+        $providers = $om->read(__CLASS__, $oids, ['partner_identity_id.address_street', 'partner_identity_id.address_city'], $lang);
+        foreach($providers as $oid => $provider) {
+            $result[$oid] = "{$provider['partner_identity_id.address_street']} {$provider['partner_identity_id.address_city']}";
+        }
+        return $result;
+    }
+
+    public static function calcVatNumber($om, $oids, $lang) {
+        $result = [];
+
+        $providers = $om->read(__CLASS__, $oids, ['partner_identity_id.vat_number'], $lang);
+        foreach($providers as $oid => $provider) {
+            $result[$oid] = "{$provider['partner_identity_id.vat_number']}";
+        }
+        return $result;
+    }
+
+    public static function calcRegistrationNumber($om, $oids, $lang) {
+        $result = [];
+
+        $providers = $om->read(__CLASS__, $oids, ['partner_identity_id.registration_number'], $lang);
+        foreach($providers as $oid => $provider) {
+            $result[$oid] = "{$provider['partner_identity_id.registration_number']}";
+        }
+        return $result;
+    }
+
+
+}
