f6b623eb8443ae7900daddec456f9ada5fed1536
Lucas <lucas@yesbabylon.com>
2025-03-19 15:55:51 +0100

    Handle internal activity

---
+++ sale/classes/booking/BookingPartnerActivity.class.php
@@ -0,0 +1,50 @@
+<?php
+/*
+    This file is part of the Discope property management software <https://github.com/discope-pms/discope>
+    Some Rights Reserved, Discope PMS, 2020-2025
+    Original author(s): Yesbabylon SRL
+    Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
+*/
+
+namespace sale\booking;
+
+use equal\orm\Model;
+
+class BookingPartnerActivity extends Model {
+
+    public static function getDescription(): string {
+        return "Custom planning activity to add notes on partners activities or partners availabilities.";
+    }
+
+    public static function getColumns(): array {
+        return [
+
+            'name' => [
+                'type'              => 'string',
+                'description'       => "Name of the activity.",
+                'required'          => true
+            ],
+
+            'partner_id' => [
+                'type'              => 'many2one',
+                'foreign_object'    => 'identity\Partner',
+                'description'       => "Employee assigned to the supervision of the activity.",
+                'required'          => true
+            ],
+
+            'activity_date' => [
+                'type'              => 'date',
+                'description'       => "Specific date on which the service is delivered.",
+                'required'          => true
+            ],
+
+            'time_slot_id' => [
+                'type'              => 'many2one',
+                'foreign_object'    => 'sale\booking\TimeSlot',
+                'description'       => "Specific day time slot on which the service is delivered.",
+                'required'          => true
+            ]
+
+        ];
+    }
+}
