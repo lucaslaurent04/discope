0fa166f7ad28777f8b535d4d7d4b4807c4ad9f30
Cédric Françoys <cedricfrancoys@gmail.com>
2025-03-23 13:28:42 +0100

    comments

--- sale/data/booking/stats/stat-nightsagerange.php
+++ sale/data/booking/stats/stat-nightsagerange.php
@@ -208,8 +208,9 @@ foreach($bookings as $booking) {
             }
 
             $age_range_id = $group_age_range_id;
-            $nb_pers = $line['qty'];
 
+            // set qty as default value for nb_pers, according to accounting method
+            $nb_pers = $line['qty'];
             if($line['product_id']['product_model_id']['qty_accounting_method'] == 'person') {
                 $nb_pers /= $group['nb_nights'];
             }
