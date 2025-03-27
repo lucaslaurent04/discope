4874853ef972e1e864ae2996b2ddbd691df8fb2f
Lucas <lucas@yesbabylon.com>
2025-03-27 11:22:06 +0100

    add description to rental units list of booked services

--- discope/apps/booking/src/app/model/rental.unit.class.ts
+++ discope/apps/booking/src/app/model/rental.unit.class.ts
@@ -4,6 +4,7 @@ export class RentalUnitClass {
     constructor(
         public id: number = 0,
         public name: string = '',
+        public description: string = '',
         public capacity: number = 0,
         public order: number = 0,
         public is_accomodation: boolean = false,
