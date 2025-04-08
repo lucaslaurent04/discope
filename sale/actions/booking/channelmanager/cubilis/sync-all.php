b8f2599b86ecb0dbd9e7491ee510cdcc2eeaa2a7
alexandraYesbabylon <alexandra@yesbabylon.com>
2025-04-08 14:49:31 +0000

    Update section booking to feature for the client_domain

--- sale/actions/booking/channelmanager/cubilis/sync-all.php
+++ sale/actions/booking/channelmanager/cubilis/sync-all.php
@@ -61,7 +61,7 @@ if(!$channelmanager_enabled) {
     throw new Exception('disabled_feature', QN_ERROR_INVALID_CONFIG);
 }
 
-$client_domain = Setting::get_value('sale', 'booking', 'channelmanager.client_domain', 'https://discope.yb.run');
+$client_domain = Setting::get_value('sale', 'features', 'channelmanager.client_domain');
 
 // #memo - prevent calls from non-production server
 if(constant('ROOT_APP_URL') != $client_domain) {
