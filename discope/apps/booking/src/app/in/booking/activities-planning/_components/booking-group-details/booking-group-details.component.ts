22795e4dcb63df31ef0ab098ca1e97669ddbead8
Lucas <lucas@yesbabylon.com>
2025-04-10 10:30:18 +0200

    Add modification of group nb pers

--- discope/apps/booking/src/app/in/booking/activities-planning/_components/booking-group-details/booking-group-details.component.ts
+++ discope/apps/booking/src/app/in/booking/activities-planning/_components/booking-group-details/booking-group-details.component.ts
@@ -1,16 +1,36 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { BookingLineGroup } from '../../_models/booking-line-group.model';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'booking-activities-planning-booking-group-details',
     templateUrl: 'booking-group-details.component.html',
     styleUrls: ['booking-group-details.component.scss']
 })
-export class BookingActivitiesPlanningBookingGroupDetailsComponent implements OnInit {
+export class BookingActivitiesPlanningBookingGroupDetailsComponent implements OnChanges {
 
     @Input() group: BookingLineGroup|null;
 
-    public ngOnInit() {
-        console.log('init BookingActivitiesPlanningBookingGroupDetailsComponent');
+    @Output() nbPersChanged = new EventEmitter<number>();
+
+    public nbPersFormControl: FormControl;
+
+    constructor(
+    ) {
+        this.nbPersFormControl = new FormControl(0, [Validators.min(1)]);
+    }
+
+    public ngOnChanges(changes: SimpleChanges) {
+        if(changes.group && this.group) {
+            this.nbPersFormControl.setValue(this.group.nb_pers);
+        }
+    }
+
+    public async onNbPersChanges() {
+        if(!this.nbPersFormControl.valid) {
+            return;
+        }
+
+        this.nbPersChanged.emit(this.nbPersFormControl.value);
     }
 }
