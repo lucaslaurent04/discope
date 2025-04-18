660fca6c48d57ac1a50e9d8eacf14341326b5063
Lucas <lucas@yesbabylon.com>
2025-04-18 16:35:06 +0200

    add total to enrollment

---
+++ sale/classes/camp/document/Document.class.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace sale\camp\document;
+
+class Document extends \documents\Document {
+
+    public static function getColumns(): array {
+        return [
+
+            'enrollment_id' => [
+                'type'              => 'many2one',
+                'foreign_object'    => 'sale\camp\Enrollment',
+                'description'       => "The enrollment the document is needed for."
+            ],
+
+            'enrollment_type' => [
+                'type'              => 'string',
+                'selection'         => [
+                    'doc-type-1', // TODO: Add real documents codes
+                    'doc-type-2',
+                    'doc-type-3',
+                    'doc-type-4',
+                    'other'
+                ],
+                'description'       => "The enrollment document type.",
+                'default'           => 'other'
+            ]
+
+        ];
+    }
+}
