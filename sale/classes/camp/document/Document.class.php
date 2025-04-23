d6508e7dd72ab349f7061ceb3137fa135ffebe4c
Lucas <lucas@yesbabylon.com>
2025-04-23 16:34:27 +0200

    add document types

--- sale/classes/camp/document/Document.class.php
+++ sale/classes/camp/document/Document.class.php
@@ -16,11 +16,9 @@ class Document extends \documents\Document {
             'enrollment_type' => [
                 'type'              => 'string',
                 'selection'         => [
-                    'doc-type-1', // TODO: Add real documents codes    "aide de la CCVG" | ""
-                    'doc-type-2',
-                    'doc-type-3',
-                    'doc-type-4',
-                    'other'
+                    'other',
+                    'help-ccvg',
+                    'health-sheet'
                 ],
                 'description'       => "The enrollment document type.",
                 'default'           => 'other'
