c4502682dad198b5561badcb18206a121e4d4a3f
Lucas <lucas@yesbabylon.com>
2025-05-30 16:08:09 +0200

    create camp app

---
+++ discope/apps/camp/src/app/in/camps/camps.component.ts
@@ -0,0 +1,80 @@
+import { Component, OnInit, AfterViewInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { ContextService } from 'sb-shared-lib';
+
+
+@Component({
+  selector: 'camps',
+  templateUrl: 'camps.component.html',
+  styleUrls: ['camps.component.scss']
+})
+export class CampsComponent implements OnInit, AfterViewInit, OnDestroy {
+
+    // rx subject for unsubscribing subscriptions on destroy
+    private ngUnsubscribe = new Subject<void>();
+
+    public ready: boolean = false;
+
+    // flag telling if the route to which the component is associated with is currently active (amongst routes defined in first parent routing module)
+    private active = false;
+
+    private default_descriptor: any = {
+        // route is current ng route
+        context: {
+            entity: 'sale\\camp\\Enrollment',
+            view: "list.default",
+            order: "id",
+            sort: "desc"
+        }
+    };
+
+    constructor(
+        private route: ActivatedRoute,
+        private context: ContextService
+    ) {}
+
+    public ngOnDestroy() {
+        console.log('CampsComponent::ngOnDestroy');
+        this.ngUnsubscribe.next();
+        this.ngUnsubscribe.complete();
+    }
+
+    public ngAfterViewInit() {
+        console.log('CampsComponent::ngAfterViewInit');
+
+        this.context.setTarget('#sb-container-camps');
+        const descriptor = this.context.getDescriptor();
+        if(!Object.keys(descriptor.context).length) {
+            console.log('CampComponent : requesting change', this.default_descriptor);
+            this.context.change(this.default_descriptor);
+        }
+
+        this.active = true;
+    }
+
+    public ngOnInit() {
+        console.log('CampsComponent::ngOnInit');
+
+        this.context.ready.pipe(takeUntil(this.ngUnsubscribe)).subscribe( (ready:boolean) => {
+            this.ready = ready;
+        });
+
+        // once view is ready, subscribe to route changes
+        this.route.params.pipe(takeUntil(this.ngUnsubscribe)).subscribe( async (params:any) => {
+            // no params for this route(/camps)
+        });
+
+        // if no context or all contexts have been closed, re-open default context (wait for route init)
+        this.context.getObservable().pipe(takeUntil(this.ngUnsubscribe)).subscribe( () => {
+            console.log('CampsComponent:: received context change');
+            const descriptor = this.context.getDescriptor();
+            if(descriptor.hasOwnProperty('context') && !Object.keys(descriptor.context).length && this.active) {
+                this.ready = false;
+                this.context.change(this.default_descriptor);
+            }
+        });
+    }
+
+}
