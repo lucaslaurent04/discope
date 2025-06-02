ad72f8c62314cb591251f41ddb4bebe1f740ef4c
Lucas <lucas@yesbabylon.com>
2025-06-02 09:55:37 +0200

    add handling of enrollment required documents

---
+++ sale/classes/camp/EnrollmentDocument.class.php
@@ -0,0 +1,46 @@
+<?php
+/*
+    This file is part of the Discope property management software <https://github.com/discope-pms/discope>
+    Some Rights Reserved, Discope PMS, 2020-2025
+    Original author(s): Yesbabylon SRL
+    Licensed under GNU AGPL 3 license <http://www.gnu.org/licenses/>
+*/
+
+namespace sale\camp;
+
+use equal\orm\Model;
+
+class EnrollmentDocument extends Model {
+
+    public static function getColumns(): array {
+        return [
+
+            'enrollment_id' => [
+                'type'              => 'many2one',
+                'foreign_object'    => 'sale\camp\Enrollment',
+                'required'          => true,
+                'readonly'          => true
+            ],
+
+            'document_id' => [
+                'type'              => 'many2one',
+                'foreign_object'    => 'sale\camp\EnrollmentDocument',
+                'required'          => true,
+                'readonly'          => true
+            ],
+
+            'received' => [
+                'type'              => 'boolean',
+                'description'       => "Has the document been received?",
+                'default'           => false
+            ]
+
+        ];
+    }
+
+    public function getUnique(): array {
+        return [
+            ['enrollment_id', 'document_id']
+        ];
+    }
+}
