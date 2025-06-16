96d5b09e9194d245519ac8be949ce98f4415aa6d
Cédric Françoys <cedricfrancoys@gmail.com>
2025-06-16 22:36:35 +0200

    restored chain of events for customer

--- sale/classes/booking/Booking.class.php
+++ sale/classes/booking/Booking.class.php
@@ -521,10 +521,6 @@ class Booking extends Model {
         ];
     }
 
-    public static function oncreate($self) {
-        $self->do('import_contacts');
-    }
-
     public static function calcName($om, $oids, $lang) {
         $result = [];
 
