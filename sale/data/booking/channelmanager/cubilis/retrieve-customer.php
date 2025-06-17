efda14e6bc4325f425f1e329a1d1cb1564523bd3
Cédric Françoys <cedricfrancoys@gmail.com>
2025-06-17 10:08:26 +0200

    improved address_country adaptation

--- sale/data/booking/channelmanager/cubilis/retrieve-customer.php
+++ sale/data/booking/channelmanager/cubilis/retrieve-customer.php
@@ -103,7 +103,7 @@ else {
             'address_street'    => $params['address_street'],
             'address_city'      => $params['address_city'],
             'address_zip'       => $params['address_zip'],
-            'address_country'   => $params['address_country'],
+            'address_country'   => substr($params['address_country'], 0, 2),
             'phone'             => str_replace(['.', '/', ' ', '-'], '', $params['phone']),
             'email'             => $params['email'],
             'lang_id'           => $lang_id,
@@ -112,7 +112,8 @@ else {
 
     // sanitize / exclude fields
 
-    if(!(preg_match('/^[A-Z]{2}$/u', $values['address_country']))) {
+    $country_iso = ['AF','AX','AL','DZ','AS','AD','AO','AI','AQ','AG','AR','AM','AW','AU','AT','AZ','BS','BH','BD','BB','BY','BE','BZ','BJ','BM','BT','BO','BA','BW','BV','BR','IO','BN','BG','BF','BI','KH','CM','CA','CV','KY','CF','TD','CL','CN','CX','CC','CO','KM','CG','CD','CK','CR','CI','HR','CU','CY','CZ','DK','DJ','DM','DO','EC','EG','SV','GQ','ER','EE','ET','FK','FO','FJ','FI','FR','GF','PF','TF','GA','GM','GE','DE','GH','GI','GR','GL','GD','GP','GU','GT','GG','GN','GW','GY','HT','HM','VA','HN','HK','HU','IS','IN','ID','IR','IQ','IE','IM','IL','IT','JM','JP','JE','JO','KZ','KE','KI','KR','KW','KG','LA','LV','LB','LS','LR','LY','LI','LT','LU','MO','MK','MG','MW','MY','MV','ML','MT','MH','MQ','MR','MU','YT','MX','FM','MD','MC','MN','ME','MS','MA','MZ','MM','NA','NR','NP','NL','AN','NC','NZ','NI','NE','NG','NU','NF','MP','NO','OM','PK','PW','PS','PA','PG','PY','PE','PH','PN','PL','PT','PR','QA','RE','RO','RU','RW','BL','SH','KN','LC','MF','PM','VC','WS','SM','ST','SA','SN','RS','SC','SL','SG','SK','SI','SB','SO','ZA','GS','ES','LK','SD','SR','SJ','SZ','SE','CH','SY','TW','TJ','TZ','TH','TL','TG','TK','TO','TT','TN','TR','TM','TC','TV','UG','UA','AE','GB','US','UM','UY','UZ','VU','VE','VN','VG','VI','WF','EH','YE','ZM','ZW'];
+    if(!in_array($values['address_country'], $country_iso)) {
        $values['address_country'] = 'BE';
     }
 
