6e6aee045a51d109bd8bb06493e8e60e80fd0812
Lucas <lucas@yesbabylon.com>
2025-07-08 15:41:17 +0200

    add stat enrollments by sojourn types

--- sale/data/camp/stats/stat-enrollments-by-sojourn-types.php
+++ sale/data/camp/stats/stat-enrollments-by-sojourn-types.php
@@ -35,6 +35,11 @@ use sale\camp\WorksCouncil;
             'description'       => "Date interval upper limit (defaults to last day of the current month).",
             'default'           => fn() => strtotime('last day of this month')
         ],
+        'by_duration' => [
+            'type'              => 'boolean',
+            'description'       => "Separate CLSH sojourn by duration",
+            'default'           => false
+        ],
         'status' => [
             'type'              => 'string',
             'description'       => "The status of the enrollments.",
@@ -54,10 +59,15 @@ use sale\camp\WorksCouncil;
             'type'              => 'string',
             'description'       => "Name of the center for the enrollments quantities."
         ],
-        'works_council_id' => [
-            'type'              => 'many2one',
-            'foreign_object'    => 'sale\camp\WorksCouncil',
-            'description'       => "The works council that was used with the enrollment."
+        'sojourn_type' => [
+            'type'              => 'string',
+            'selection'         => [
+                'camp',
+                'clsh',
+                'clsh-4-days',
+                'clsh-5-days',
+            ],
+            'description'       => "The camp sojourn type that was used with the enrollment."
         ],
         'qty' => [
             'type'              => 'integer',
@@ -105,54 +115,46 @@ $result = [];
 
 $camps = Camp::search($domain)
     ->read([
-        'center_id' => [
-            'name'
-        ],
+        'is_clsh',
+        'clsh_type',
+        'center_id',
         'enrollments_ids' => [
-            'status',
-            'works_council_id'
+            'status'
         ]
     ])
     ->get(true);
 
-$map_center_works_councils_enrollments_qty = [];
-$map_works_councils = [];
+$map_center_sojourn_types_enrollments_qty = [];
 
 foreach($camps as $camp) {
+    $sojourn_type = $camp['is_clsh'] ? 'clsh' : 'camp';
+    if($sojourn_type === 'clsh' && $params['by_duration']) {
+        $sojourn_type = $camp['clsh_type'] === '4-days' ? 'clsh-4-days' : 'clsh-5-days';
+    }
+
     foreach($camp['enrollments_ids'] as $enrollment) {
         if($params['status'] !== 'all' && $enrollment['status'] !== $params['status']) {
             continue;
         }
 
-        if(!isset($enrollment['works_council'])) {
-            continue;
+        if(!isset($map_center_sojourn_types_enrollments_qty[$camp['center_id']])) {
+            $map_center_sojourn_types_enrollments_qty[$camp['center_id']] = [];
+        }
+        if(!isset($map_center_sojourn_types_enrollments_qty[$camp['center_id']][$sojourn_type])) {
+            $map_center_sojourn_types_enrollments_qty[$camp['center_id']][$sojourn_type] = 0;
         }
 
-        if(!isset($map_center_works_councils_enrollments_qty[$camp['center_id']['id']])) {
-            $map_center_works_councils_enrollments_qty[$camp['center_id']['id']] = [];
-        }
-        if(!isset($map_center_works_councils_enrollments_qty[$camp['center_id']['id']][$enrollment['works_council']])) {
-            $map_center_works_councils_enrollments_qty[$camp['center_id']['id']][$enrollment['works_council']] = 0;
-        }
-
-        $map_center_works_councils_enrollments_qty[$camp['center_id']['id']][$enrollment['works_council']]++;
-        $map_works_councils[$enrollment['works_council']] = true;
+        $map_center_sojourn_types_enrollments_qty[$camp['center_id']][$sojourn_type]++;
     }
 }
 
-$center_ids = array_keys($map_center_works_councils_enrollments_qty);
+$center_ids = array_keys($map_center_sojourn_types_enrollments_qty);
 
 $centers = Center::search(['id', 'in', $center_ids])
     ->read(['name'])
     ->get();
 
-$works_councils_ids = array_keys($map_works_councils);
-
-$works_councils = WorksCouncil::search(['id', 'in', $works_councils_ids])
-    ->read(['name'])
-    ->get();
-
-foreach($map_center_works_councils_enrollments_qty as $center_id => $map_works_councils_qty) {
+foreach($map_center_sojourn_types_enrollments_qty as $center_id => $map_sojourn_types_qty) {
     $center = null;
     foreach($centers as $c) {
         if($c['id'] === $center_id) {
@@ -161,19 +163,11 @@ foreach($map_center_works_councils_enrollments_qty as $center_id => $map_works_c
         }
     }
 
-    foreach($map_works_councils_qty as $works_council_id => $qty) {
-        $works_council = null;
-        foreach($works_councils as $wc) {
-            if($wc['id'] === $works_council_id) {
-                $works_council = $wc['name'];
-                break;
-            }
-        }
-
+    foreach($map_sojourn_types_qty as $sojourn_type => $qty) {
         $result[] = [
-            'center'            => $center,
-            'works_councils'    => ['id' => $works_council_id, 'name' => $works_council],
-            'qty'               => $qty
+            'center'        => $center,
+            'sojourn_type'  => $sojourn_type,
+            'qty'           => $qty
         ];
     }
 }
