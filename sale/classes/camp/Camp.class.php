a932d921ed04d1e3a44afc4b845641b2e7116dd0
Lucas <lucas@yesbabylon.com>
2025-07-15 09:45:52 +0200

    Add check editable fields for published camps

--- sale/classes/camp/Camp.class.php
+++ sale/classes/camp/Camp.class.php
@@ -644,8 +644,17 @@ class Camp extends Model {
         ]);
 
         foreach($self as $camp) {
-            if($camp['status'] === 'published') {
-                return ['status' => ['already_published' => "A published camp can't be modified."]];
+            if($camp['status'] !== 'published') {
+                continue;
+            }
+
+            $allowed_fields = [
+                'status', 'remarks', 'public_description', 'employee_ratio', 'max_children',
+                'enrollments_qty', 'camp_group_qty', 'ase_quota', 'camp_groups_ids',
+                'enrollments_ids', 'booking_activities_ids', 'presences_ids', 'booking_meals_ids'
+            ];
+            if(count(array_diff(array_keys($values), $allowed_fields)) > 0) {
+                return ['status' => ['non_editable' => "This field can't be modified on a published camp."]];
             }
         }
 
