007aa3177a7ec661b6f5efedc8c30503dbf7c76d
Lucas <lucas@yesbabylon.com>
2025-07-22 10:59:07 +0200

    Add room renting when not accommodation to print arrivals

--- sale/data/booking/print-arrivals.php
+++ sale/data/booking/print-arrivals.php
@@ -264,6 +264,7 @@ foreach($bookings as $booking) {
         'member'            => lodging_booking_print_booking_formatMember($booking),
         'contacts'          => [],
         'sojourns'          => [],
+        'rental_units'      => [],
         'customer'          => $booking['customer_identity_id'],
         'price'             => $booking['price'],
         'paid_amount'       => 0.0,
@@ -311,6 +312,23 @@ foreach($bookings as $booking) {
                 }
             }
             $item['sojourns'][] = $sojourn;
+        }
+        else {
+            $rental_units = [];
+            foreach($group['sojourn_product_models_ids'] as $spm) {
+                if(!$spm['is_accomodation']) {
+                    foreach($spm['rental_unit_assignments_ids'] as $assignment) {
+                        $rental_units[] = [
+                            'code'  => $assignment['rental_unit_id']['code'],
+                            'name'  => $assignment['rental_unit_id']['name'],
+                            'qty'   => $assignment['qty']
+                        ];
+                    }
+                }
+            }
+            if(!empty($rental_units)) {
+                $item['rental_units'] = array_merge($item['rental_units'], $rental_units);
+            }
         }
     }
 
