b74ad32bf561d2ceea7fed858b209b663cbde14e
Lucas <lucas@yesbabylon.com>
2025-07-22 11:04:31 +0200

    handle rental unit renting in sojourn when not accommodation

--- sale/data/booking/print-arrivals.php
+++ sale/data/booking/print-arrivals.php
@@ -302,11 +302,18 @@ foreach($bookings as $booking) {
             ];
 
             foreach($group['sojourn_product_models_ids'] as $spm) {
-                if($spm['is_accomodation']) {
-                    foreach($spm['rental_unit_assignments_ids'] as $assignment) {
+                foreach($spm['rental_unit_assignments_ids'] as $assignment) {
+                    if($spm['is_accomodation']) {
                         $sojourn['rental_units'][] = [
                             'name'   => $assignment['rental_unit_id']['code'],
                             'qty'    => $assignment['qty']
+                        ];
+                    }
+                    else {
+                        $rental_units[] = [
+                            'code'  => $assignment['rental_unit_id']['code'],
+                            'name'  => $assignment['rental_unit_id']['name'],
+                            'qty'   => $assignment['qty']
                         ];
                     }
                 }
