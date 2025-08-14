2840f271fdc2853b0e0636b12b5e0b3ecab213b7
Lucas <lucas@yesbabylon.com>
2025-08-14 14:54:08 +0200

    add proforma status to booking classes + handle it in partner planning mail

--- sale/classes/booking/Booking.class.php
+++ sale/classes/booking/Booking.class.php
@@ -219,7 +219,8 @@ class Booking extends Model {
                     'validated',                // signed contract and first installment have been received
                     'checkedin',                // host is currently occupying the booked rental unit
                     'checkedout',               // host has left the booked rental unit
-                    'invoiced',
+                    'proforma',                 // balance invoice created but not emitted yet
+                    'invoiced',                 // balance invoice emitted
                     'debit_balance',            // customer still has to pay something
                     'credit_balance',           // a reimbursement to customer is required
                     'balanced'                  // booking is over and balance is cleared
