5cc7bdb9f074e9fbaa45f0ae37cf2df7c95b2e4c
Lucas <lucas@yesbabylon.com>
2025-08-28 12:27:55 +0200

    handle groups permissions

--- sale/actions/camp/sponsor/invoice.php
+++ sale/actions/camp/sponsor/invoice.php
@@ -41,7 +41,7 @@ use sale\camp\Sponsor;
     ],
     'access'        => [
         'visibility'        => 'protected',
-        'groups'            => ['camp.default.user'],
+        'groups'            => ['camp.default.administrator'],
     ],
     'response'      => [
         'content-type'      => 'application/pdf',
