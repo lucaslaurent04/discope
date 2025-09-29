aa7afaa6e195d3f8b68750ce1f21324a3da4c8d4
Cédric Françoys <cedricfrancoys@gmail.com>
2025-09-29 15:14:57 +0200

    fixed filter on status

--- sale/data/camp/collect-pending-camps.php
+++ sale/data/camp/collect-pending-camps.php
@@ -49,19 +49,8 @@ $result = [];
 
 $domain = new Domain($params['domain']);
 
-$domain->addCondition(new DomainCondition('status', '=', 'published'));
+$domain->addCondition(new DomainCondition('status', '<>', 'published'));
 
-if(isset($params['date_from'])) {
-    $domain->addCondition(
-        new DomainCondition('date_from', '>=', $params['date_from'])
-    );
-}
-
-if(isset($params['date_to'])) {
-    $domain->addCondition(
-        new DomainCondition('date_from', '<=', $params['date_to'])
-    );
-}
 
 $params['domain'] = $domain->toArray();
 
